2e2cdd2871c97d56681e302e1bc4328e78a80bcf
urzuae <urzuae@gmail.com>
2013-07-29 03:10:55 -0500

    Move htaccess file and update sql for new table

--- sql/herbalife.sql
+++ sql/herbalife.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.28, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: herbalife
 -- ------------------------------------------------------
--- Server version	5.5.28-0ubuntu0.12.04.2
+-- Server version	5.5.32-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,34 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Companion`
+--
+
+DROP TABLE IF EXISTS `Companion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Companion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `nationality` varchar(255) NOT NULL,
+  `relative` varchar(255) NOT NULL,
+  `passport` varchar(255) NOT NULL,
+  `passport_due_date` date NOT NULL,
+  `visa` varchar(255) NOT NULL,
+  `visa_due_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Companion`
+--
+
+/*!40000 ALTER TABLE `Companion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Companion` ENABLE KEYS */;
 
 --
 -- Table structure for table `Users`
@@ -62,4 +90,4 @@ CREATE TABLE `Users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-07-28  1:44:17
+-- Dump completed on 2013-07-29  3:05:32
