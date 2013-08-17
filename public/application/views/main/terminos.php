b66f87e4dee5cd3dded479de93164503597ca909
urzuae <urzuae@gmail.com>
2013-08-17 01:13:22 -0500

    Givestyles to terms and conditions pagew

--- public/application/views/main/terminos.php
+++ public/application/views/main/terminos.php
@@ -2,112 +2,52 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="<?php echo base_url('statics/css/general.css'); ?>" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url('statics/css/terms.css'); ?>" type="text/css" />
     <script type="text/javascript" src="<?php echo base_url('statics/js/jquery.js'); ?>"></script>
-    <!--<script type="text/javascript" src="http://localhost/validamesta/src/validamesta.js"></script>-->
-    <script type="text/javascript">
-      /*$(document).ready(function() {
-        //$('#contact_info').validamesta();
-        $('#contact_info').validamesta();
-        $('#contact_info').submit(function() { 
-          $(this).data('validamesta').validate();
-          return false;
-        });
-      });*/
-    </script>
   </head>
   <body>
-    <div id="container">
-      <header>
-        <a href="<?php echo base_url(); ?>">
-          <img src="<?php echo base_url('statics/images/misc/herbalife.png'); ?>" alt="herbalife" class="herbalife"/>
-          <img src="<?php echo base_url('statics/images/misc/logoHerbalife.png'); ?>" alt="herbalife" class="logo"/></a>
-      </header>
-      TÉRMINOS Y CONDICIONES
+    <header>
+      <a href="http://herbalife.hen/">
+        <img src="http://herbalife.hen/statics/images/misc/herbalife.png" alt="herbalife" class="herbalife" />
+        <img src="http://herbalife.hen/statics/images/misc/logoHerbalife.png" alt="herbalife" class="logo"/></a>
+    </header>
+    <section> 
+      <h1>TÉRMINOS Y CONDICIONES</h1>
+      <ol>
+        <li>Esta es solo una cotización la cual <span>no garantiza ningún servicio</span>, aplica únicamente para las <span>Vacaciones LATinolvidables 2013</span> a realizarse en Cancún, Quintana Roo del 19 al 24 de Noviembre de 2013.</li>
+        <li>Esta cotización es <span>solo por servicio de Hospedaje y Transportación Terrestre</span> en la ciudad de Cancún (Aeropuerto-Hotel-Aeropuerto) en las fechas antes mencionadas.</li>
+        <li>
+          Solo será válido una vez que Meeting Planners International verifique el pago correspondiente a la solicitud de adicionales. <span>No se aceptan parcialidades.</span>
+          <p>Los precios mencionados son en <span>dólares americanos (USD)</span> y exclusivos para los distribuidores registrados. Se puede pagar con:</p>
+          <ul>
+            <li>Tarjeta de Crédito (Vía PayPal)</li>
+            <li>Deposito Bancario:
+              <ul>
+                <li>Razón Social: Meeting Planners International S.A. de C.V.</li>
+                <li>Banco Beneficiario: BMSXMXMM Banco Santander S.A.</li>
+                <li>Cuenta Abono: 92-00107952-4</li>
+                <li>CLABE: 014180920010795249</li>
+                <li>SWIFT/ABA: 02100021</li>
+                <li>Moneda: Dólares Americanos</li>
+              </ul>
+            </li>
+          </ul>
+          <p>Enviarnos copia de su comprobante de pago a <a href="mailto:pagos@vacacioneslatinolvidables.com">pagos@vacacioneslatinolvidables.com</a></p>
+        </li>
+        <li>Las cotizaciones descritas tendrán vigencia hasta <span>15 días naturales</span> a partir de la fecha de emisión de este documento. Una vez transcurrido el tiempo límite establecido, los precios están sujetos a cambio sin previo aviso.</li>
+        <li>En la cotización usted encontrará expresamente descritos y desglosados todos los servicios y/o productos que le ofreceremos. <span>Cualquier servicio no mencionado, no será efectuado.</span></li>
+        <li>Cuando el servicio no haya sido concluido por motivos imputables al cliente, los y <span>pagos no serán reembolsados.</span> Solamente serán reembolsados los pagos cuando la falta sea ocasionada por motivos imputables a Meeting Planners International S.A. de C.V. y siempre que el cliente lo solicite por escrito.</li>
+      </ol>
 
- 
-
-1.- Esta es solo una cotización la cual no garantiza ningún servicio, aplica únicamente para las Vacaciones LATinolvidables 2013 a realizarse en Cancún, Quintana Roo del 19 al 24 de Noviembre de 2013.
-
- 
-
-2.- Esta cotización es solo por servicio de Hospedaje y Transportación Terrestre en la ciudad de Cancún (Aeropuerto-Hotel-Aeropuerto) en las fechas antes mencionadas.
-
- 
-
-3.- Solo será válido una vez que Meeting Planners International verifique el pago correspondiente a la solicitud de adicionales. No se aceptan parcialidades.
-
- 
-
-Los precios mencionados son en dólares americanos (USD) y exclusivos para los distribuidores registrados. Se puede pagar con:
-
- 
-
-·          Tarjeta de Crédito (Vía PayPal)
-
-·          Deposito Bancario:
-
- 
-
-o    Razón Social:                        Meeting Planners International S.A. de C.V.
-
-o    Banco Beneficiario:              BMSXMXMM Banco Santander S.A.
-
-o    Cuenta Abono:                   92-00107952-4
-
-o    CLABE:                                   014180920010795249
-
-o    SWIFT/ABA:                            02100021
-
-o    Moneda:                                               Dólares Americanos
-
- 
-
-Enviarnos copia de su comprobante de pago a  pagos@vacacioneslatinolvidables.com
-
- 
-
-4.- Las cotizaciones descritas tendrán vigencia hasta 15 días naturales a partir de la fecha de emisión de este documento. Una vez transcurrido el tiempo límite establecido, los precios están sujetos a cambio sin previo aviso.
-
- 
-
-5.- En la cotización usted encontrará expresamente descritos y desglosados todos los servicios y/o productos que le ofreceremos. Cualquier servicio no mencionado, no será efectuado.
-
- 
-
-6.- Cuando el servicio no haya sido concluido por motivos imputables al cliente, los y pagos no serán reembolsados. Solamente serán reembolsados los pagos cuando la falta sea ocasionada por motivos imputables a Meeting Planners International S.A. de C.V. y siempre que el cliente lo solicite por escrito.
-
-AVISO DE PRIVACIDAD
-
- 
-
-Meeting Planners International S.A. de C.V., con domicilio en Platón 414, Polanco, 11560, Distrito Federal, México, es responsable de recabar sus datos personales, del uso que se le dé a los mismos y de su protección. Sus datos personales serán utilizados para las siguientes finalidades: Facturación, Registro de Asistencia a Eventos, Renta de Habitaciones, Paquetes Vacacionales y Transportación.
-
- 
-
-Para las finalidades señaladas en el presente aviso de privacidad, podemos recabar sus datos personales de distintas formas: Vía electrónica (web, e-mail, chat, encuestas en línea, etc.) o Telefónicamente.
-
- 
-
-Para las finalidades antes mencionadas, requerimos obtener los siguientes datos personales: Nombre, Apellido Paterno, Apellido Materno, Teléfono, Celular, E-mail, R.F.C., Edad, Sexo, Estado Civil, Fecha de Nacimiento, Numero de Empleado, Estado y Ciudad de Residencia así como los mismos datos de sus Acompañantes.
-
- 
-
-Usted tiene derecho de acceder, rectificar y cancelar sus datos personales, así como de oponerse al tratamiento de los mismos o revocar el consentimiento que para tal fin nos haya otorgado, a través de los procedimientos que hemos implementado. Para conocer dichos procedimientos, los requisitos y plazos, se puede poner en contacto con nuestro departamento de datos personales en Platón 414, Polanco 11560 Distrito Federal México, al teléfono en la ciudad de México +52 (55) 5281 0040, o por correo electrónico avisodeprivacidad@mpi.mx, o visitar nuestra página de Internet www.mpimexico.com
-
- 
-
-Asimismo, le informamos que sus datos personales pueden ser transferidos y tratados dentro y fuera del país, por personas distintas a esta empresa. En ese sentido, su información puede ser compartida con Hoteles, Transportadoras, Centros de Convenciones, Centros de Espectáculos, Líneas Aéreas, Operadores Turísticos y se utilizaran para el Registro de servicios solicitados especificas.
-
- 
-
-Si usted no manifiesta su oposición para que sus datos personales sean transferidos, se entenderá que ha otorgado su consentimiento para ello.
-
-Si usted desea dejar de recibir mensajes promocionales de nuestra parte puede solicitarlo a través del correo electrónico avisodeprivacidad@mpi.mx, o en el siguiente domicilio Platón 414, Polanco, 11560, Distrito Federal, México.
-
- 
-
-Cualquier modificación a este aviso de privacidad podrá consultarla en www.mpimexico.com
-    </div>
+      <h2>AVISO DE PRIVACIDAD</h2>
+      <p>Meeting Planners International S.A. de C.V., con domicilio en Platón 414, Polanco, 11560, Distrito Federal, México, es responsable de recabar sus datos personales, del uso que se le dé a los mismos y de su protección. Sus datos personales serán utilizados para las siguientes finalidades: Facturación, Registro de Asistencia a Eventos, Renta de Habitaciones, Paquetes Vacacionales y Transportación.</p>
+      <p>Para las finalidades señaladas en el presente aviso de privacidad, podemos recabar sus datos personales de distintas formas: Vía electrónica (web, e-mail, chat, encuestas en línea, etc.) o Telefónicamente.</p>
+      <p>Para las finalidades antes mencionadas, requerimos obtener los siguientes datos personales: Nombre, Apellido Paterno, Apellido Materno, Teléfono, Celular, E-mail, R.F.C., Edad, Sexo, Estado Civil, Fecha de Nacimiento, Numero de Empleado, Estado y Ciudad de Residencia así como los mismos datos de sus Acompañantes.</p>
+      <p>Usted tiene derecho de acceder, rectificar y cancelar sus datos personales, así como de oponerse al tratamiento de los mismos o revocar el consentimiento que para tal fin nos haya otorgado, a través de los procedimientos que hemos implementado. Para conocer dichos procedimientos, los requisitos y plazos, se puede poner en contacto con nuestro departamento de datos personales en Platón 414, Polanco 11560 Distrito Federal México, al teléfono en la ciudad de México +52 (55) 5281 0040, o por correo electrónico avisodeprivacidad@mpi.mx, o visitar nuestra página de Internet <a href="http://www.mpimexico.com">www.mpimexico.com</a></p>
+      <p>Asimismo, le informamos que sus datos personales pueden ser transferidos y tratados dentro y fuera del país, por personas distintas a esta empresa. En ese sentido, su información puede ser compartida con Hoteles, Transportadoras, Centros de Convenciones, Centros de Espectáculos, Líneas Aéreas, Operadores Turísticos y se utilizaran para el Registro de servicios solicitados especificas.</p>
+      <p>Si usted no manifiesta su oposición para que sus datos personales sean transferidos, se entenderá que ha otorgado su consentimiento para ello.</p>
+      <p>Si usted desea dejar de recibir mensajes promocionales de nuestra parte puede solicitarlo a través del correo electrónico <a href="mailto:avisodeprivacidad@mpi.mx">avisodeprivacidad@mpi.mx</a>, o en el siguiente domicilio Platón 414, Polanco, 11560, Distrito Federal, México.</p>
+      <p>Cualquier modificación a este aviso de privacidad podrá consultarla en <a href="http://www.mpimexico.com">www.mpimexico.com</a></p>
+    </section>
   </body>
 </html>
